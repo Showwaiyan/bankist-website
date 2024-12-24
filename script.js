34324d0beece51876c5ec3b7cfe509a3b8880530
Show Wai Yan <showwaiyan555@gmail.com>
2024-12-24 14:27:31 +0800

    Feature: add tab components functinality

--- script.js
+++ script.js
@@ -71,3 +71,19 @@ document.querySelector('.nav__links').addEventListener('click',(e)=>{
         document.querySelector(e.target.getAttribute("href")).scrollIntoView({behavior: 'smooth'});
     }
 })
+
+// Tap Component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabContainer = document.querySelector(".operations__tab-container");
+const tabContent = document.querySelectorAll(".operations__content");
+
+tabContainer.addEventListener('click',(e)=>{
+    const clicked = e.target.closest(".operations__tab");
+    if (!clicked) return;
+
+    tabs.forEach(t=>t.classList.remove('operations__tab--active'));
+    tabContent.forEach(c=>c.classList.remove('operations__content--active'));
+
+    clicked.classList.add('operations__tab--active');
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add("operations__content--active");
+})
